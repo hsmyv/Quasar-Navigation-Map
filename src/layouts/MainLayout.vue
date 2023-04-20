593bffb2858bf76be2930fd3bc70e7ef8a202ca4
Hasan <hsmusayev@gmail.com>
2023-04-20 17:14:29 +0400

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,28 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header class="bg-teal">
+      <q-toolbar>
+        <q-toolbar-title>
+          HS Map
+        </q-toolbar-title>
+
+        <q-btn flat round dense icon="login"/>
+      </q-toolbar>
+    </q-header>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer elevated>
+      <q-tabs class="text-light bg-teal">
+      <q-tab name="/map" icon="explore" label="Map"/>
+      <q-tab name="/route" icon="directions" label="Routes"/>
+      <q-tab name="/auth" icon="login" label="Auth"/>
+      </q-tabs>
+    </q-footer>
+  </q-layout>
+</template>
+
+<script setup>
+
+</script>
---
+++ src/router/routes.js
@@ -0,0 +1,27 @@
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      {
+        path: '/route',
+        component: () => import('pages/RoutePage.vue'),
+        children: [
+          { path: '', component: () => import('pages/route/RouteSelect.vue') },
+          { path: 'add-route', component: () => import('pages/route/AddRoute.vue') },
+          { path: 'saved-routes', component: () => import('pages/route/SavedRoutes.vue') }
+        ]
+      }
+    ]
+  },
+
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: '/:catchAll(.*)*',
+    component: () => import('pages/ErrorNotFound.vue')
+  }
+]
+
+export default routes
